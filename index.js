089945992fc28a6d9cf4b3fd0994cbb043a2dbf2
Nick Landas <nick.landas@gmail.com>
2022-11-01 10:48:57 -0500

    commit tree 5e755ddcbd4c638e88f81715de262d2a16a15f20

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, name){
+    const result = array.filter(elem => elem.toLowerCase() === name.toLowerCase());
+    return result;
+}
+
+
+function fuzzyMatch(array, partName){
+    const result = array.filter(elem => elem.substring(0,2) === partName);
+    return result;
+}
+
+
+
+function matchName(array, string){
+    const result = array.filter(elem => elem.name === string);
+    return result;
+}
